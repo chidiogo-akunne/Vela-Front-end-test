45b52a1af8e98798716f062055ec133387306839
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 00:14:18 +0100

    create index view

---
+++ src/View/Index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import NavBar from '../Components/NavBar/NavBar'
+
+export default function Index() {
+    return (
+        <div>
+            <NavBar/>
+        </div>
+    )
+}
