a9e1a0973c0b42126eebe41a6b4397628750a2c7
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-22 22:44:17 +0100

    create search input component

---
+++ src/Components/Commons/SearchInput/SearchInput.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function SearchInput(props) {
+  return (
+    <div>
+      <form class="form-inline my-2 my-lg-0">
+        <input
+          class="form-control mr-sm-2"
+          type="search"
+          placeholder={props.placholder}
+          aria-label="Search"
+        />
+      </form>
+    </div>
+  );
+}
