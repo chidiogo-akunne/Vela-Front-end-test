98bbf70f2ee67bbf42508b46839ffacd38a4026f
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 04:40:32 +0100

    remove span and add merchant profile link

--- src/Components/SideNav/SideNav.js
+++ src/Components/SideNav/SideNav.js
@@ -1,53 +1,72 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Button from "../Commons/Button/Button";
+import "./SideNav.css";
 
 export default function SideNav() {
   return (
-    <div>
-      <Button className="btn btn-success">GENERATE INVOICE</Button>
-      <ul>
-        <span>
-          <li>Main</li>
-          <li>
-            <i class="fas fa-tachometer-alt"></i>
-            <Link to="/"></Link>
-          </li>
-        </span>
-        <span>
-          <li>Payments</li>
-          <li>
-            <i class="fas fa-wallet"></i>
-            <Link to="/">All Payments</Link>
-          </li>
-          <li>
-            <i class="fas fa-wallet"></i>
-            <Link to="/">Reconciled Payment</Link>
-          </li>
-          <li>
-            <i class="fas fa-wallet"></i>
-            <Link to="/">Un-Reconciled Payment</Link>
-          </li>
-          <li>
-            <i class="fas fa-dot-circle"></i>
-            <Link to="/">Manuel Settlement</Link>
-          </li>
-        </span>
-        <span>
-          <li>Orders</li>
-          <li>
-            <i class="far fa-list-alt"></i>
-            <Link to="/">All orders</Link>
-          </li>
-          <li>
-            <i class="far fa-list-alt"></i>
-            <Link to="/">Pending orders</Link>
-          </li>
-          <li>
-            <i class="far fa-list-alt"></i>
-            <Link to="/">Reconciled orders</Link>
-          </li>
-        </span>
+    <div className="sideNav">
+      <Button className="btn btn-success mx-auto d-flex">
+        GENERATE INVOICE
+      </Button>
+      <ul className="sideNav-Ul">
+        <li className="sideNav-Space">Main</li>
+        <li>
+          <i class="fas fa-tachometer-alt"></i>
+          <Link to="/" className="sideNav-Link">
+            Overview
+          </Link>
+        </li>
+        <li className="sideNav-Space">Payments</li>
+        <li>
+          <i class="fas fa-wallet"></i>
+          <Link to="/" className="sideNav-Link">
+            All Payments
+          </Link>
+        </li>
+        <li>
+          <i class="fas fa-wallet"></i>
+          <Link to="/" className="sideNav-Link">
+            Reconciled Payment
+          </Link>
+        </li>
+        <li>
+          <i class="fas fa-wallet"></i>
+          <Link to="/" className="sideNav-Link">
+            Un-Reconciled Payment
+          </Link>
+        </li>
+        <li>
+          <i class="fas fa-dot-circle"></i>
+          <Link to="/" className="sideNav-Link">
+            Manuel Settlement
+          </Link>
+        </li>
+        <li className="sideNav-Space">Orders</li>
+        <li>
+          <i class="far fa-list-alt"></i>
+          <Link to="/" className="sideNav-Link">
+            All orders
+          </Link>
+        </li>
+        <li>
+          <i class="far fa-list-alt"></i>
+          <Link to="/" className="sideNav-Link">
+            Pending orders
+          </Link>
+        </li>
+        <li>
+          <i class="far fa-list-alt"></i>
+          <Link to="/" className="sideNav-Link">
+            Reconciled orders
+          </Link>
+        </li>
+        <li className="sideNav-Space">
+          <i class="far fa-user"></i>
+          <Link to="/" className="sideNav-Link">
+            Merchant Profile
+          </Link>
+        </li>
       </ul>
     </div>
   );
