781a4adbb23fe13f5f9dcccf51ef3c2339e28f06
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 16:59:40 +0100

    refactor code

--- src/Components/TableTransaction/TableTransaction.js
+++ src/Components/TableTransaction/TableTransaction.js
@@ -1,34 +1,48 @@
-import React from "react";
+import React, {useState} from "react";
 import Data from "../../Data/data";
 import Button from "../Commons/Button/Button";
+import './TableTransaction.css'
 
-export default function TableTransaction() {
+export default function TableTransaction(props) {
+  const [transaction, setTransaction] = useState([
+    {status: "Reconciled", status: "Pending", status: "Un-reconciled"}
+  ]);
   return (
-    <div>
+    <div className={props.className}>
       <table class="table">
         <thead class="thead-light">
           <tr>
-            <th scope="col">Image Type</th>
+            <th scope="col">Item Type</th>
             <th scope="col">Price</th>
             <th scope="col">Transaction No</th>
             <th scope="col">Time</th>
             <th scope="col">Status</th>
           </tr>
         </thead>
-        <tbody>
+        <tbody className="tableBody">
           {Data.map(product => (
             <tr>
               <td>
-                <span>VW</span> {product.item}
+                <span className="badgePill mr-3">VW</span> {product.item}
               </td>
               <td>{product.price}</td>
               <td>{product.transaction}</td>
               <td>{product.time}</td>
               <td>
-                <Button className="btn btn-outline-secondary">
-                  {product.status}
+                {product.status === "Reconciled" ? (
+                  <Button className="btn btn-outline-secondary reconciled">
+                  <i class="fas fa-circle"></i> Reconciled
+                  </Button>
+                ): product.status === "Un-reconciled" ? (
+                  <Button className="btn btn-outline-secondary unreconciled">
+                <i class="fas fa-circle"></i> Un-reconciled
                 </Button>
-                <i class="fas fa-angle-down"></i>
+                ) : (
+                  <Button className="btn btn-outline-secondary pending">
+                <i class="fas fa-circle"></i> Pending
+                </Button>
+                )}
+                <i class="fas fa-angle-down pl-3"></i>
               </td>
             </tr>
           ))}
@@ -36,4 +50,12 @@ export default function TableTransaction() {
       </table>
     </div>
   );
+}
+
+const reconciled = {
+    color: 'yellow'
+}
+
+const pending = {
+    color: 'blue'
 }
