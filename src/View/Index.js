c8c760f6783e10ff7b2722449bd40d5d662f4738
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 05:46:35 +0100

    add daily transaction components

--- src/View/Index.js
+++ src/View/Index.js
@@ -1,6 +1,8 @@
-import React from 'react'
+import React from 'react';
+import './Index.css';
 import NavBar from '../Components/NavBar/NavBar'
 import SideNav from '../Components/SideNav/SideNav'
+import DailyTransactions from '../Components/DailyTransactions/DailyTransactions'
 
 export default function Index() {
     return (
@@ -9,6 +11,26 @@ export default function Index() {
             <div className="row">
                 <div className="col-3">
                     <SideNav/>
+                </div>
+                <div className="col-9">
+                    <div className="container">
+                        <div className="row index-Row">
+                            <div className="col-lg-3 col-sm-6">
+                                <DailyTransactions title="Volume" price="2,342"/>
+                            </div>
+                            <div className="col-lg-3 col-sm-6">
+                                <DailyTransactions title="Value" price="4,000,000"/>
+                            </div>
+                            <div className="col-lg-3 col-sm-6">
+                                <DailyTransactions title="Volume" price="452,000"/>
+                            </div>
+                            <div className="col-lg-3 col-sm-6">
+                                <DailyTransactions title="Value" price="4,000,000"/>
+                            </div>
+                        </div>
+
+
+                    </div>
                 </div>
             
             
