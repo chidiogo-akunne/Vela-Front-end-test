cd6ccb13eb4d788a6b686531c20dc4047ec6fec4
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 03:40:22 +0100

    refactor code

--- src/Components/Commons/SearchInput/SearchInput.js
+++ src/Components/Commons/SearchInput/SearchInput.js
@@ -1,16 +1,19 @@
 import React from "react";
+import "./SearchInput.css";
 
 export default function SearchInput(props) {
   return (
     <div>
-      <form class="form-inline my-2 my-lg-0">
+      <form className="form-inline my-2 my-lg-0 fas fa-search">
         <input
           className="form-control mr-sm-2"
           type="search"
-          placeholder={props.placholder}
+          placeholder={props.placeholder}
           aria-label="Search"
+          className={props.SearchStyle}
         />
       </form>
     </div>
   );
 }
+
