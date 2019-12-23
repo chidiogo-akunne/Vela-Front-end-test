dc4ad1d8c9b2f841a5496afef0057096704a77f3
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 04:41:01 +0100

    add side nav component

--- src/View/Index.js
+++ src/View/Index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import NavBar from '../Components/NavBar/NavBar'
+import SideNav from '../Components/SideNav/SideNav'
 
 export default function Index() {
     return (
         <div>
             <NavBar/>
+            <div className="row">
+                <div className="col-3">
+                    <SideNav/>
+                </div>
+            
+            
+            </div>
         </div>
     )
 }
