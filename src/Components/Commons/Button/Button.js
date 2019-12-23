dc8e99aef98de6e785a242b8cba2c23e6cf33257
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 04:39:47 +0100

    refactor code

--- src/Components/Commons/Button/Button.js
+++ src/Components/Commons/Button/Button.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Button (props) {
     return (
-        <button type="button" className={props.ClassName} style={props.ButtonStyle}>{props.children}</button>
+        <button type="button" className={props.className} style={props.ButtonStyle}>{props.children}</button>
     )
 }
