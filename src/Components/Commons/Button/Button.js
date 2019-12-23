99528ab5dd15ce6b4a60bfbb3798d074304c385b
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 06:39:11 +0100

    add data-toggle

--- src/Components/Commons/Button/Button.js
+++ src/Components/Commons/Button/Button.js
@@ -2,6 +2,6 @@ import React from 'react';
 
 export default function Button (props) {
     return (
-        <button type="button" className={props.className} style={props.ButtonStyle}>{props.children}</button>
+        <button type="button" className={props.className} style={props.ButtonStyle} data-toggle={props.dataToggle}>{props.children}</button>
     )
 }
