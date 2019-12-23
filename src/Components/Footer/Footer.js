929346813c6e0a9c7c02da4e0cc3488335298db5
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 22:27:01 +0100

    make footer mobile responsive

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import Pagination from '../Pagination/Pagination'
+import Pagination from '../Pagination/Pagination';
+import './Footer.css';
 
 export default function Footer() {
     return (
-        <div className="row">
+        <div className="row footer-Q">
            <div className="col-6">
            <p>Showing 1 to 10 of 500 enteries 
             </p>
