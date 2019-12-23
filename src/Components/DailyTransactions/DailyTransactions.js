47ed17dc8b849b6f0b04ccb2bfd82f2a73d7bd71
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-23 22:26:18 +0100

    make daily transaction component responsive

--- src/Components/DailyTransactions/DailyTransactions.js
+++ src/Components/DailyTransactions/DailyTransactions.js
@@ -1,18 +1,19 @@
 import React from "react";
 import './DailTransactions.css';
 import Card from "../Commons/Card/Card";
+import SmallChart from "../Charts/SmallChart";
 
 export default function DailyTransactions(props) {
   return (
     <div>
       <Card style={transactionCardStyle}>
         <div className="row">
-          <div className="col-sm-8">
+          <div className="col-8">
             <p className="transaction-P">Daily Transaction {props.title}</p>
             <p className="transaction-Price"><span>&#8358;</span>{props.price}</p>
           </div>
-          <div className="col-sm-4">
-              Chart
+          <div className="col-4">
+              <SmallChart />
           </div>
         </div>
       </Card>
