c247b1995faf19a1fb364beacadf56012ed6e88e
chidiogo-akunne <chidiogoakunne@gmail.com>
2019-12-24 09:52:08 +0100

    create chart with victory chart

--- src/Components/Charts/SmallChart.js
+++ src/Components/Charts/SmallChart.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import {VictoryArea, VictoryChart, VictoryGroup} from 'victory';
+import './SmallCharts.css';
+
 export default function SmallChart() {
+
       return (
-        <VictoryChart width={400} height={400}>
+        <VictoryChart 
+        width={400} 
+        height={400}
+        style = {{
+          line: {display: 'none'}
+        }}
+        >
           <VictoryGroup
             style={{
               data: { strokeWidth: 3, fillOpacity: 0.4 }
@@ -10,7 +19,7 @@ export default function SmallChart() {
           >
             <VictoryArea
               style={{
-                data: { fill: "blue", stroke: "blue" }
+                data: { fill: "rgb(93, 157, 225)", stroke: "rgb(93, 157, 225)" },
               }}
               data={[
                 { x: 1, y: 2 },
